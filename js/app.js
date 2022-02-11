d178bf58e954b6b5907a708d55d2e6926201f55e
charlesjbasedow <cbasedow@outlook.com>
2022-02-11 14:08:31 -0500

    fix button phrasing

--- js/app.js
+++ js/app.js
@@ -15,9 +15,9 @@ const playerHandEl = document.getElementById('player-hand')
 const dealerHandEl = document.getElementById('dealer-hand')
 const playerHandValueEl = document.getElementById('player-value')
 const dealerHandValueEl = document.getElementById('dealer-value')
-const playbtn = document.getElementById('playbtn')
-const hitbtn = document.getElementById('hitbtn')
-const standbtn = document.getElementById('standbtn')
+const playbtn = document.getElementById('play-btn')
+const hitbtn = document.getElementById('hit-btn')
+const standbtn = document.getElementById('stand-btn')
 const statusMsg = document.querySelector('.game-status')
 
 /*----------------------------- Event Listeners -----------------------------*/
