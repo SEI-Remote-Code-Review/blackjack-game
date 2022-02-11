7c6b81d641b28c56f5e964c1c9434f3309d560d1
charlesjbasedow <cbasedow@outlook.com>
2022-02-11 11:34:53 -0500

    define variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('blackjack testing')
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+let cardDeck = []
+let playerHand = []
+let dealerHand = []
+let playerHandValue = 0
+let dealerHandValue = 0
+let playerTurn = null
+let gameWinner = null 
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
