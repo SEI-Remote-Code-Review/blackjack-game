304f5d18b770e69a906a14866f6ad4bd666cab58
charlesjbasedow <cbasedow@outlook.com>
2022-02-14 10:03:47 -0500

    hit btn add

--- js/app.js
+++ js/app.js
@@ -21,7 +21,8 @@ const standbtn = document.getElementById('stand-btn')
 const statusMsg = document.querySelector('.game-status')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+playbtn.addEventListener('click', playGame)
+hitbtn.addEventListener('click, htiFunc')
 
 
 /*-------------------------------- Functions --------------------------------*/
@@ -30,4 +31,13 @@ init ()
 
 function init() {
   cardDeck = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+}
+console.log(cardDeck)
+
+function playGame() {
+  if (cardDeck.length > 0) {
+    let randIdx = Math.floor(Math.random() * cardDeck.length)
+
+
+  }
 }
